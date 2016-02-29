17e17d4a4f6990ca096bd8e1ca1cec810ef289b4
Marc <phyrenight@gmail.com>
2016-02-29 15:54:45 -0500

    first commit

---
+++ vocab_db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Foreignkey, Integer, String
+form sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy umport create_enigine
+
+Base = declarative_base()
+
+class Dict(Base):
+    __tablename__ = 'Dict'
+    id = Column(Integer, primary_key=True)
+    word = Column(String(30), nullable=True)
+    definition = Column(String(250), nullable=True)
+
+
+    @property
+    def serialize(self):
+        return {
+                'id': self.id,
+                'word': self.word,
+                'definition': self.definition,
+               }
+
+engine = create_engine('sql:///Dictionary.db')
+Base.metadata.create_all(engine)
